e3c5b2c92261ef6338449fc20f7bd275357435a6
Santhosh <sanmax2016@gmail.com>
2023-04-30 22:55:12 +0530

    Finished

---
+++ app/src/main/java/com/example/cameraapp/MainActivity2.kt
@@ -0,0 +1,61 @@
+package com.example.cameraapp
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+@Suppress("DEPRECATION")
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var imgPhoto: ImageView
+    private val permissionRequestCamera = 101
+
+    private val takePictureResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val data = result.data
+            val photo = data?.extras?.get("data") as Bitmap
+            imgPhoto.setImageBitmap(photo)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        imgPhoto = findViewById(R.id.imgPhoto)
+        val btTakePhoto: Button = findViewById(R.id.btTakePhoto)
+        findViewById(R.id.reset)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), permissionRequestCamera)
+        }
+
+        btTakePhoto.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            takePictureResultLauncher.launch(intent)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == permissionRequestCamera) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "Error!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
